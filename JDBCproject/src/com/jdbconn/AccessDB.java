35310dfb135c4813a56a4cbb763f67087cf930dc
Ashish <Ashish@192.168.1.45>
2023-07-24 12:37:16 +0530

    JDBC1

---
+++ JDBCproject/src/com/jdbconn/AccessDB.java
@@ -0,0 +1,18 @@
+package com.jdbconn;
+
+import java.sql.Connection;
+
+public class AccessDB {
+	public static Connection con=null;
+	
+	public AccessDB()
+	{
+		con=DBConnection.getConnection();
+		System.out.println("connection established....");
+	}
+	
+	public static void main(String args[])
+	{
+		AccessDB obj=new AccessDB();
+	}
+}
